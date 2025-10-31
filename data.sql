07961070c5c9ba02854ccad5de527cf281bc0265
lobo-k4 <leandromont94@gmail.com>
2025-10-31 10:13:14 -0400

    Create data.sql

---
+++ data.sql
@@ -0,0 +1,25 @@
+/*ingresar los datos personales*/
+
+insert into users (ci, nombre, fecha_nacimiento, email, edad, password)
+
+values
+
+(54846858, 'Douglas', '22/02/2002', 'douglaspere@gmail.com','23', 'DouglasP23');
+
+insert into users (ci, nombre, fecha_nacimiento, email, edad, password)
+
+values
+
+(54413858, 'Lucero', '12/08/2000', 'luceropere@gmail.com','33', 'LuceroP22');
+
+insert into users (ci, nombre, fecha_nacimiento, email, edad, password)
+
+values
+
+(85646846, 'Damaris', '10/09/1987', 'damarisalvarado@gmail.com','63', 'DamarisP13');
+
+insert into users (ci, nombre, fecha_nacimiento, email, edad, password)
+
+values
+
+(5484647, 'Douglas', '22/02/2002', 'douglaspere@gmail.com','23', 'DouglasP23')
